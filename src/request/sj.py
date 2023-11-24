6f1828500eeddc8d5e3576baad09fb94c8a11a23
nickllama <llama.gradual-0j@icloud.com>
2023-11-24 11:43:54 +0100

    - Спрятал чувствительные данные. - Обновил Readme файл исходя из последних изменений. -добавил файлы зависимости в toml

--- src/request/sj.py
+++ src/request/sj.py
@@ -1,9 +1,11 @@
-from config import URL_SJ
-from src.data_json.work_with_json import WorkWithJson
 import requests
 import dotenv
+import os
+from config import URL_SJ
+from src.data_json.work_with_json import WorkWithJson
 
-config = dotenv.dotenv_values(".env")
+
+dotenv.load_dotenv(".env")
 
 
 class RequestsSJ(WorkWithJson):
@@ -16,7 +18,7 @@ class RequestsSJ(WorkWithJson):
     def request(self) -> dict:
         """Запрос"""
         headers = {
-            "X-Api-App-Id": "v3.r.112492319.a7c4a3d27213114ba6c67347ae3a6264084122a7.19153f63812efb61654ff50fcdf7d91e1c1c7cfb"
+            "X-Api-App-Id": os.getenv("SJ_API_TOKEN")
         }
         responce = requests.get(self.url, params=self.params, headers=headers)
         return WorkWithJson.save_json(responce.json()["objects"])
